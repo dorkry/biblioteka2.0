bb6901dc92941046999c615c74d1520ab70a8387
Dorota Krygowska <dorota.krygowska@gmail.com>
2021-05-25 22:59:09 +0200

    dziala

--- biblioteka/__init__.py
+++ biblioteka/__init__.py
@@ -1,26 +1,22 @@
-# app/__init__.py
+#__init__
 
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-from . import models
+from . import models, routes
 
 @app.shell_context_processor
 def make_shell_context():
-   return {
-       "db": db,
-       "Author": models.Author,
-       "Book": models.Book
-   }
-
-
-
-
+  return {
+      "db": db,
+      "Author": models.Author,
+      "Book": models.Book,
+      "Borrow":models. Borrow
+  }
